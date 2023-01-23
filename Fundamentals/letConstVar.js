384071a2dc6c44165fe29712021c374bc830fc9e
xzqt23 <angelo.agualada@gmail.com>
2023-01-23 21:59:56 +0800

    first commit

---
+++ Fundamentals/letConstVar.js
@@ -0,0 +1,40 @@
+//let,const and var
+
+
+/*
+
+****let***
+Variables defined with let cannot be Redeclared.
+
+Variables defined with let must be Declared before use.
+
+It can be reassigned
+
+Variables defined with let have Block Scope.
+
+
+***const***
+
+Variables defined with const cannot be Redeclared.
+
+Variables defined with const cannot be Reassigned.
+
+Variables defined with const have Block Scope.
+
+It should be initialized with a value.
+
+
+***var***
+If you want your code to run in older browsers, you must use var.
+
+The var keyword is used in all JavaScript code from 1995 to 2015.
+
+
+-------------------------------------------------
+As best practice use const keyword when declaring a variable and only
+use let when the value of the variable will change.
+-------------------------------------------------
+
+avoid using variable name because this is reserved in JS
+*/
+
